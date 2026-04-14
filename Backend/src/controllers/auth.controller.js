498ed4ec843e29f553d04afc5a6633840437cb53
AshishManer <108808771+ash44444@users.noreply.github.com>
2026-04-14 19:25:52 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -88,7 +88,11 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,        // ✅ required on HTTPS (Render)
+  sameSite: "None",    // ✅ REQUIRED for cross-origin
+});
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
