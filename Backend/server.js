19b16ee22e892167a96519306322a4c21c84bb46
AshishManer <108808771+ash44444@users.noreply.github.com>
2026-04-14 16:38:37 +0530

    Add health check endpoint to serverfirst

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,10 @@ const connectToDB = require("./src/config/database");
 
 connectToDB();
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req,res) => {
+  res.send(`Server is running`);
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
